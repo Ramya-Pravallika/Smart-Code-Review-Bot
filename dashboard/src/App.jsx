83c59f2702f4f4eed515d523eac9ba0fa85f4d87
Ramya Pravallika <107994772+Ramya-Pravallika@users.noreply.github.com>
2025-10-16 12:17:26 +0530

    Initial commit: Dashboard app

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>SMART Code Review Bot Dashboard</h1>
+      {/* Add stats, review logs, configuration options */}
+    </div>
+  );
+}
+
+export default App;
