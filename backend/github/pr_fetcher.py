380c616ee4123f666efd931c0146ee955b6c8a4e
Ramya Pravallika <107994772+Ramya-Pravallika@users.noreply.github.com>
2025-10-16 12:15:50 +0530

    Initial commit: PR code fetcher stub

---
+++ backend/github/pr_fetcher.py
@@ -0,0 +1,7 @@
+import requests
+
+def fetch_pr_code(pr_url):
+    # Use GitHub API to get changed files in PR
+    # Return list of file contents or paths
+    # Here is a stub
+    return ["def foo(): pass  # example code"]
